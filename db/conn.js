2e6ff4110cc53b24fcb9d055aac735ae975c6cb1
KaranPannu668 <karanpannu668@gmail.com>
2021-11-18 18:40:33 +0530

    Update conn.js

--- db/conn.js
+++ db/conn.js
@@ -1,15 +1,5 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.DB_CONNECTION_MONGO_ATLAS,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    //useCreateIndex: true,
-    autoIndex: false,
-    connectTimeoutMS: 0,
-    socketTimeoutMS: 0,
-    family: 4
-    }).then(()=>{
-        console.log("Connection successful");
-    }).catch((e)=>{
-        console.log("Connection unsuccessful"+e);
-    })
+mongoose.connect(DB_CONNECTION_MONGO_ATLAS)
+    .then(() => console.log("connection successfull........."))
+    .catch((err) => console.log(err));
