86f8bfda1876c871ecbb9ba7c720aee5a18f8110
KaranPannu668 <karanpannu668@gmail.com>
2021-12-12 22:44:11 +0530

    Minor fix

--- db/conn.js
+++ db/conn.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.LOCALHOST,{
+mongoose.connect(process.env.MONGOATLAS,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
     //useCreateIndex: true,
